ae1913eb4658a40d652760a6af29010b769de4aa
despiow <anthony.jardy@gmail.com>
2023-05-30 09:51:53 +0200

    mise a jour de synchro cours et test2 pour insertion dans la bdd des cours

--- synchro_cours.php
+++ synchro_cours.php
@@ -1,39 +1,93 @@
 <?php
-session_start();
-require('bdd.php');
-ini_set('display_errors', 1);
-ini_set('display_startup_errors', 1);
-error_reporting(E_ALL);
-// Récupérer la liste des fichiers dans le répertoire
-$dir    = '/var/www/html/projet_sn_bts_anthony/calendriers';
-$files = scandir($dir, SCANDIR_SORT_DESCENDING);
-$files = array_filter($files,static function ($element)
-    {
-    	$check_a = $element !== '.';
-    	$check_b = $element !== '..';
-    	return $check_a && $check_b;
+    require_once '/var/www/html/projet_sn_bts_anthony/Projet_bts2023/vendor/autoload.php';
+    require_once("bdd.php");
+    ini_set('display_errors', 1);
+    ini_set('display_startup_errors', 1);
+    error_reporting(E_ALL);
+    use ICal\ICal;
+    $dir = '/var/www/html/projet_sn_bts_anthony/Projet_bts2023/calendriers/';
+    $files = scandir($dir, SCANDIR_SORT_ASCENDING);
+    $files = array_filter($files, static function ($element) {
+        $check_a = $element !== '.';
+        $check_b = $element !== '..';
+        return $check_a && $check_b;
     });
+    $sql = "SELECT id, nom FROM salle";
+    $result = query($sql);
+    $ids_salles = array();
+    if ($result->num_rows > 0) {
+        while ($row = $result->fetch_assoc()) {
+            $ids_salles[] = $row;
+        }
+    }
+    conn_end($conn);
 
-// Boucler sur la liste des fichiers
-$sql_query = 'INSERT INTO `cours` (`id_salle`, `nom`,`prof`,`classe`,`jour`,`heure_debut`,`heure_fin`) VALUES ';
-foreach ($files as $ics_filname) 
-{
-  // Insérer le nom de la salle et le nom du fichier dans une base de données ou un fichier texte
-  $nom_salle = str_replace("Emploi_du_Temps_","",$ics_filname);
-  $nom_salle = str_replace(".ics","",$nom_salle);
-  $nom_fichier = $ics_filname;
-  $sql = "('".$nom_salle."', '".$nom_fichier."'),";
-  $sql_query .= $sql;
-}
-$sql_query = substr($sql_query,0,-1);
-$sql_query .= ';';
-$result = query($sql_query);
-  if(mysqli_affected_rows($conn)>=1){
-      echo "Ajout réussi de ".mysqli_affected_rows($conn)." salle(s)";
-  }else if(mysqli_errno($conn)==1062){
-      echo "Identifiant déjà utilisé";
-  }else{
-      echo "Ajout échoué : ".mysqli_errno($conn);
-  }
-  conn_end($conn);
+    function find_id_salle_by_name($name, $array)
+    {
+        foreach ($array as $item) {
+            if ($item['nom'] == $name) {
+                return $item['id'];
+            }
+        }
+        return null; // Returns null if the name is not found
+    }
+    foreach ($files as $file) {
+        try {
+            $ical = new ICal($dir . $file, array(
+                'defaultSpan' => 2,     // Default value
+                'defaultTimeZone' => 'UTC',
+                'defaultWeekStart' => 'MO',  // Default value
+                'disableCharacterReplacement' => false, // Default value
+                'filterDaysAfter' => null,  // Default value
+                'filterDaysBefore' => null,  // Default value
+                'httpUserAgent' => null,  // Default value
+                'skipRecurrence' => false, // Default value
+            ));
+        } catch (\Exception $e) {
+            echo $e;
+            die();
+        }
+        echo "events : ".$ical->eventCount;
+        $status_failed = 0;
+        $status_success = 0;
+        foreach ($ical->events() as $event) {
+            if (isset($event->summary)) {
+                $summary_elements = count(explode(" - ", $event->summary));
+                $info_cours = array();
+                if ($summary_elements > 1) {
+                    $matiere = null;
+                    foreach (explode("\n", $event->description) as $item) {
+                        if (strpos($item, "Matière") !== false) {
+                            $colonPos = strpos($item, ':');
+                            $modifiedItem = substr($item, $colonPos + 2);
+                            $matiere = $modifiedItem;
+                        }
+                    }
+                    $matiere = $matiere;
+                    $nom_salle = str_replace(" ", "_", explode(",", $event->location)[0]);
+                    $jour = date("Y-m-d", $event->dtstart_array[2]);
+                    $heure_debut = date("H:i:s", $event->dtstart_array[2]);
+                    $heure_fin = date("H:i:s", $event->dtend_array[2]);
+                    $id_salle = find_id_salle_by_name($nom_salle, $ids_salles);
+                    $sql = "INSERT IGNORE INTO cours (id_salle, nom, jour, heure_debut, heure_fin) VALUES (?, ?, ?, ?, ?);";
+                    $stmt = $mysqli->prepare($sql);
+                    $stmt->bind_param("issss", $id_salle, $matiere, $jour, $heure_debut, $heure_fin);
+                    $status = $stmt->execute();
+                    if($status){
+                        $status_success++;
+                    }else{
+                        $status_failed++;
+                    }
+                }
+            }
+        }
+        if ($mysqli->affected_rows >= 1) {
+            echo "Ajout réussi de " . $status_success . " cours(s)";
+            echo "Ajout échoué de " . $status_failed . " cours(s)";
+        } else if ($mysqli->errno == 1062) {
+            echo "Identifiant déjà utilisé";
+        } else {
+            echo "Ajout échoué : " . $mysqli->error;
+        }
+    }
 ?>
